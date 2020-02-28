2ad114f3c873acd541a47aab964382fc9414017f
Pontus Tahir <pontus.tahir@gmail.com>
2020-02-28 09:49:08 +0100

    layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ProfileImg from './ProfileImg'
+import Header from './Header'
+
+const Layout = () => {
+    return (
+   
+      <div>
+           <ProfileImg />
+        <Header />
+        <h1>Coz i´m a steampunker yes i am!</h1>
+        <p>really?</p>
+      </div>
+    );
+  };
+  
+  export default Layout;
