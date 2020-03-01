f2af9aa468269d5de15e23723bfe3e20f05da2f6
Pontus Tahir <pontus.tahir@gmail.com>
2020-03-01 15:47:38 +0100

    shot

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,17 +2,11 @@ import React from "react";
 import ProfileImg from "./ViewContainer/ProfileImg";
 import Header from "./Header";
 
-// interface Profile {
-//     img: string
-//     name: string
-//     todos: string[]
-// }
 
 const Layout = () => {
   return (
     <div>
       <ProfileImg />
-      <Header />
       <Header />
     </div>
   );
--- src/components/ViewContainer/ProfileView.tsx
+++ src/components/ViewContainer/ProfileView.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Todo from './Todo'
+
 
 const ProfileView = () => {
 return (
     <div>
-       <Todo />
+       ProfileView
     </div>
 )
 }
