6ccb3ef172b71bf0133fab4c4e4a2fb0198319be
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-03-02 08:58:04 +0100

    starting on QuestionForm for the MainView

--- src/components/ViewContainer/QuestionForm.tsx
+++ src/components/ViewContainer/QuestionForm.tsx
@@ -1,13 +1,45 @@
 import React from "react";
-import SubmitButton from "./SubmitButton";
+import {CSSProperties} from 'react'
+
+/* interface QuestionForm {
+  htmlFor: string;
+} */
+
 
 const QuestionForm = () => {
   return (
-    <form>
-      <h1>3 st frågor med radiobuttons till varje fråga</h1>
-      <SubmitButton>Done</SubmitButton>
-    </form>
+    <form style={questionForm}>
+      <p>Här ska fråga nummer ett vara</p>
+      <input type="radio" id="Q1Yes" name="QuestionForm" value="Yes" />
+      <label htmlFor="Q1">Ja</label>
+      <input type="radio" id="Q1No" name="QuestionForm" value="No" />
+      <label htmlFor="Q1">Nej</label>
+      <br />
+  
+      <p>Här ska fråga nummer två vara</p>
+      <input type="radio" id="Q2Yes" name="QuestionForm" value="Yes" />
+      <label htmlFor="Q2">Ja</label>
+      <input type="radio" id="Q2No" name="QuestionForm" value="No" />
+      <label htmlFor="Q2">Nej</label>
+      <br />
+
+
+      <p>Här ska fråga nummer tre vara</p>
+      <input type="radio" id="Q3Yes" name="QuestionForm" value="Yes" />
+      <label htmlFor="Q3">Ja</label>
+      <input type="radio" id="Q3No" name="QuestionForm" value="No" />
+      <label htmlFor="Q3">Nej</label>
+      <br />
+</form>
+
+  
   );
 };
+
+const questionForm: CSSProperties = {
+textAlign: 'center',
+width: '100%'
+}
+
 
 export default QuestionForm;
