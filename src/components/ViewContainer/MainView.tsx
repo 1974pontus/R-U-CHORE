91073bf08accc0a4fd8837a530751265949cec4b
Pontus Tahir <pontus.tahir@gmail.com>
2020-03-04 15:09:36 +0100

    mainview prints leo

--- src/components/ViewContainer/MainView.tsx
+++ src/components/ViewContainer/MainView.tsx
@@ -1,6 +1,6 @@
-import React, { ChangeEvent, MouseEvent } from 'react';
+import React from 'react';
 import QuestionForm from "./QuestionForm";
-import FormField from './FormField';
+
 
 interface Props {
     onProfilSelected: (profile: string) => void
@@ -8,18 +8,11 @@ interface Props {
 
 const MainView = (props: Props) => {
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        console.log(e + 'handlechange')
-    }
-
-    const handleSubmit = (e: MouseEvent<HTMLButtonElement>) => {
-        console.log(e + 'handleSubmit')
-    }
     
 return (
     <React.Fragment>
     <QuestionForm onSubmit={props.onProfilSelected}/>
-    <FormField type={"text"}onChange={handleChange} inputplaceholder={'Whats´s your name'} value="" onClick={handleSubmit} buttontext={'SUBMIT'}/>
+    
 
     </React.Fragment>
 )
