1dd952fa7057ee7404e243038c405833425be464
Pontus Tahir <pontus.tahir@gmail.com>
2020-03-06 11:27:03 +0100

    done

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,8 +2,8 @@
 import React, { CSSProperties, useState } from "react";
 import Header from './ViewContainer/Header'
 import Title from './ViewContainer/Title';
-import MainView from "./ViewContainer/MainView";
-// import ProfileView from "./ViewContainer/ProfileView";
+// import MainView from "./ViewContainer/MainView";
+import ProfileView from "./ViewContainer/ProfileView";
 import ProfileImg from "./ViewContainer/ProfileImg";
 
 //tagit bort footer från layout, footer ligger i MainView och ska läggas in i PofileView?/AddTodoForm?
@@ -18,22 +18,22 @@ import ProfileImg from "./ViewContainer/ProfileImg";
 // }
 
 const Layout = () => {
-    const [profile, setProfile] = useState('')
+//     const [profile, setProfile] = useState('')
 
-    const handleProfileSelected = (profile: string) => {
-      setProfile(profile)
-    }
-/* Klicka på submit-knappen och du får profilen loggad i consolen */
-    console.log(profile)
+//     const handleProfileSelected = (profile: string) => {
+//       setProfile(profile)
+//     }
+// /* Klicka på submit-knappen och du får profilen loggad i consolen */
+//     console.log(profile)
     return (
    
       <div>
         <ProfileImg imageSrc={require('../img/Leonardo-Dicaprio.jpg')} alt='leo DiCaprio at his best'/>
         <Header imageSrc={require('../img/space.jpg')} alt='stardust in space'/>
         <Title name="Här ska 2 olika titel vara"/>
-        <MainView onProfilSelected={handleProfileSelected}/>
+        {/* <MainView onProfilSelected={handleProfileSelected}/> */}
         <div style={todolist}>
-          {/* <ProfileView /> */}
+          <ProfileView />
           </div>
          <div style={marginbottom}>.</div>
 
