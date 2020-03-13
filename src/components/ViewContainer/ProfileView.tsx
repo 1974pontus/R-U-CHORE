3bc3b1eec859a8157d4f1f2379a39c21e28ec640
Pontus Tahir <pontus.tahir@gmail.com>
2020-03-13 10:10:19 +0100

    profileImg

--- src/components/ViewContainer/ProfileView.tsx
+++ src/components/ViewContainer/ProfileView.tsx
@@ -3,14 +3,13 @@ import TodoList from "./TodoList";
 import { ToggleTodo, AddTodo } from "./types";
 import FormField from "./FormField";
 import { ProfileData } from "./ProfileData";
-import Title from "./Title"
+import Title from "./Title";
 
 interface Props {
-  profile: ProfileData
+  profile: ProfileData;
 }
 
-
-const ProfileView: React.FC<Props> = (props) => {
+const ProfileView: React.FC<Props> = props => {
   const [todos, setTodos] = useState(props.profile.initialTodos);
 
   const toggleTodo: ToggleTodo = selectedTodo => {
@@ -34,19 +33,20 @@ const ProfileView: React.FC<Props> = (props) => {
   // let profile: {this.props.onSubmit(profileData[0]}
 
   return (
-      <React.Fragment>
-        <Title titleContent={props.profile.titel}/>
-        <TodoList todos={todos} toggleTodo={toggleTodo} />
-      
-        <FormField
-          onSubmit={addTodo}
-          buttonText="submit"
-          placeHolder="Add your todos"
-        />
-      
-      </React.Fragment>
+    <React.Fragment>
+      <Title titleContent={props.profile.titel} />
+      <TodoList 
+        todos={todos} 
+        toggleTodo={toggleTodo}
+      />
+
+      <FormField
+        onSubmit={addTodo}
+        buttonText="submit"
+        placeHolder="Add your todos"
+      />
+    </React.Fragment>
   );
 };
-
 
 export default ProfileView;
--- src/components/ViewContainer/TodoList.tsx
+++ src/components/ViewContainer/TodoList.tsx
@@ -13,7 +13,11 @@ const TodoList: React.FC<TodoListProps> = ({ todos, toggleTodo }) => {
     <ul style={centerTodos} >
       {todos.map(todo => {
         return (
-        <TodoListItem key={todo.text} todo={todo} toggleTodo={toggleTodo} />
+        <TodoListItem 
+          key={todo.text} 
+          todo={todo} 
+          toggleTodo={toggleTodo} 
+        />
         )
       })}
     </ul>
